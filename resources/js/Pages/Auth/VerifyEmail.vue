9255fd0b59b1e277193b184e5591bab7e034e669
gabrielfasano <gabriel.fasano@he-arc.ch>
2021-12-22 22:37:00 +0100

    update gitignore, update verifyEmail view

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -2,17 +2,17 @@
     <Head title="Email Verification" />
 
     <div class="mb-4 text-sm text-gray-600">
-        Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+        Bonjour et bienvenue dans l'équipe ! Un compte a été crée à votre nom. Cliquez sur le bouton ci-dessous pour recevoir un email qui vous donnera accès à lapplication.
     </div>
 
     <div class="mb-4 font-medium text-sm text-green-600" v-if="verificationLinkSent" >
-        A new verification link has been sent to the email address you provided during registration.
+        Le lien d'accès a été envoyé par mail.
     </div>
 
     <form @submit.prevent="submit">
         <div class="mt-4 flex items-center justify-between">
             <BreezeButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Resend Verification Email
+                Envoyer le lien d'accès
             </BreezeButton>
 
             <Link :href="route('logout')" method="post" as="button" class="underline text-sm text-gray-600 hover:text-gray-900">Log Out</Link>
